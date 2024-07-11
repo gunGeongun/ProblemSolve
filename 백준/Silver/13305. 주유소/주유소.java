9a94c6d8bce14eb53e9d96c5f3a89592bae8dfd1
Geon Kim <41728760+gunGeongun@users.noreply.github.com>
2024-07-11 23:39:06 +0900

    [Silver III] Title: 주유소, Time: 360 ms, Memory: 39296 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -7,39 +7,26 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n=  Integer.parseInt(br.readLine());
-        int[] distance = new int[n-1];
+        long[] distance = new long[n-1];
 
-        int[] cost = new int[n-1];
-        int[] costcpy = new int[n-1];
+        long[] cost = new long[n];
 
         String[] di = br.readLine().split(" ");
         for(int i=0;i<n-1;i++){
-            distance[i] = Integer.parseInt(di[i]);
+            distance[i] = Long.parseLong(di[i]);
         }
         di = br.readLine().split(" ");
-        for(int i=0;i<n-1;i++){
-            cost[i] = Integer.parseInt(di[i]);
+        for(int i=0;i<n;i++){
+            cost[i] =Long.parseLong(di[i]);
         }
-
-        System.arraycopy(cost,0,costcpy,0,n-1);
-        Arrays.sort(cost);
-        int min = cost[0];
-        int i =0;
-        int result =0;
-        while(true){
-            if(costcpy[i] != min){
-                result += (costcpy[i]*distance[i]);
-                i++;
+        long min = cost[0];
+        long result =0;
+        for(int i =0;i<n-1;i++){
+            if(cost[i]<min){
+                min = cost[i];
             }
-            else{
-                while(i<costcpy.length){
-                    result += (min*distance[i]);
-                    i++;
-                }
-                break;
-            }
+            result += (min * distance[i]);
         }
         System.out.println(result);
     }
-
 }
