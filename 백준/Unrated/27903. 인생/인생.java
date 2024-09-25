2fcebd308cd1d98bed7e1a97a899ee07bf5124e8
Geon Kim <41728760+gunGeongun@users.noreply.github.com>
2024-09-25 14:19:56 +0900

    [Unrated] Title: 인생, Time: 92 ms, Memory: 13888 KB -BaekjoonHub

---
+++ 백준/Unrated/27903. 인생/인생.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        char first = (char)107;
+        int six = 3 + 3;
+
+        int nine = 4+5;
+
+
+
+        sb.append(first);
+        sb.append(first);
+        sb.append(six);
+        sb.append(six);
+        sb.append(nine);
+        sb.append(six);
+        System.out.print(sb);
+
+
+    }
+
+
+}
